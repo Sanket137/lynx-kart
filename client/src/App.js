9ae0703b0fe360694fc2952e281af699618bad35
sanket <Sanket137>
2022-11-18 12:18:18 +0530

    Initial Updates and setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">Lynx-cart</a>
+      </header>
+      <main>
+        list product here
+      </main>
+    </div>
+  );
+}
+
+export default App;
